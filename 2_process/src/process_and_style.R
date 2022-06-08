c340e7fbc184807f577ecfb1e1da66a0be57e001
Anthony Martinez <ajmartinez@usgs.gov>
2022-06-08 07:36:31 -0700

    remove unnecessary . argument

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -3,7 +3,7 @@ process_data <- function(filein, site_filename){
   
   nwis_data_clean <- rename(filein, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, -tz_cd) %>%
-    left_join(., site_info, by = "site_no") %>% 
+    left_join(site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va) %>%
     mutate(station_name = as.factor(station_name))
   return(nwis_data_clean)
