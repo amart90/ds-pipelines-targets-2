3541d1a44580eb08a1f1309085f30c62f1ef7ffe
Anthony Martinez <ajmartinez@usgs.gov>
2022-06-08 07:52:35 -0700

    Change filein to nwis_data
    Co-authored-by: Lindsay (Carr) Platt <lplatt@usgs.gov>

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,7 +1,7 @@
 process_data <- function(nwis_data, site_filename){
   site_info <- read_csv(site_filename, show_col_types = FALSE)
   
-  nwis_data_clean <- rename(filein, water_temperature = X_00010_00000) %>% 
+  nwis_data_clean <- rename(nwis_data, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, -tz_cd) %>%
     left_join(site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va) %>%
