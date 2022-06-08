5b0dd2ffc09d9f96fba4f08ec01aa40dbb1f9424
Anthony Martinez <ajmartinez@usgs.gov>
2022-06-08 12:39:53 -0700

    Change target outputs

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,6 +1,6 @@
 # Download site data from USGS site file site
 # Save output as CSV
-download_nwis_site_data <- function(fileout, site_num, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
+download_nwis_site_data <- function(site_num, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
 
   # readNWISdata is from the dataRetrieval package
   data_out <- readNWISdata(sites=site_num, service="iv", 
@@ -13,20 +13,21 @@ download_nwis_site_data <- function(fileout, site_num, parameterCd = '00010', st
   }
   # -- end of do-not-edit block
   
-  write_csv(data_out, file = fileout)
-  return(fileout)
+  return(data_out)
 
 }
 
 # Read download CSVs and bind them into a single data frame
-#input_csv argument should be a vector of nwis data csv file paths
-combine_csvs <- function(filein){
-  lapply(filein, read_csv, show_col_types = FALSE) %>% 
-    bind_rows()
+#input_csv argument should be a list of nwis site data in dataframes (or similar)
+combine_data <- function(input, fileout){
+    bind_rows(input) %>%
+    write_csv(file = fileout)
+  return(fileout)
 }
 
 # Download site characteristic data
 nwis_site_info <- function(fileout, site_data){
+  site_data <- read_csv(site_data, show_col_types = FALSE)
   site_no <- unique(site_data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, fileout)
